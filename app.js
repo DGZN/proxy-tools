fbc97d335ccac6c53452742da6fa4b4271654e1d
Keiichi Lindley <digizn@gmail.com>
2018-09-19 05:09:13 -0700

    Implement import proxy list

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 const ProxyTools = require('./main')
-
-ProxyTools.isAlive('http://google.com').then((res) => {
- console.log(`[\u2714] CONNECTED`);
-}).catch(err => console.error(err.message))
+ProxyTools.import('./hosts.txt')
--- main.js
+++ main.js
@@ -2,12 +2,32 @@ class ProxyTools {
 
 	static isAlive(url, cb) {
 		return new Promise((resolve, reject) => {
-			require('request')(url,  (err, res) => {
+			require('request')(url, {timeout: 5000}, (err, res) => {
 				return err ? reject(err) : resolve(res)
 	    });
 		})
 	}
 
+	static import(file) {
+		const stream = require('stream')
+		const hosts = []
+		var instream = require('fs').createReadStream(file);
+		var outstream = new stream;
+		var rl = require('readline').createInterface(instream, outstream);
+		rl.on('line', function(line) {
+			if( !/^#/.exec(line) ) {
+				hosts.push({ target: line })
+			}
+		}.bind(this))
+		.on('close', function () {
+			hosts.map(function(host, i) {
+				this.isAlive(host.target).then(function(res) {
+					hosts[i].status = true
+					console.log(`[\u2714] ${host.target}`)
+				}).catch(err => {})
+			}.bind(this))
+		}.bind(this))
+	}
 }
 
 module.exports = ProxyTools
